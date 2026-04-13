37265b6901f2f70368cb01b618ae8e0e678fd6c0
Ishaq Ahmed <231743863+iahmed70@users.noreply.github.com>
2026-04-12 22:09:06 -0400

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+                List<List<String>> answer = new ArrayList<List<String>>();
+                        HashMap<String,Integer> data = new HashMap<>();
+                                for (int i = 0; i < strs.length; i++){
+                                            int [] letters = new int[26];
+                                                        for (int j = 0; j < strs[i].length(); j++){
+                                                                        letters[((int) strs[i].charAt(j))-97] +=1;
+                                                                                    }
+                                                                                                if (data.containsKey(Arrays.toString(letters))){
+                                                                                                                answer.get(data.get(Arrays.toString(letters))).add(strs[i]);
+                                                                                                                            }
+                                                                                                                                        else {
+                                                                                                                                                        List<String> temp = new ArrayList<String>();
+                                                                                                                                                                        temp.add(strs[i]);
+
+                                                                                                                                                                                                        answer.add(temp);
+                                                                                                                                                                                                                        data.put(Arrays.toString(letters), answer.size() - 1); //            
+                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                System.out.println("+++" + answer);
+                                                                                                                                                                                                                                                                        return answer; 
+    }
+}
